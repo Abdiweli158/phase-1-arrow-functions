431edd5466688af8bf8622c41a581e32b6d736ff
Abdiweli158 <walicade158@gmail.com>
2023-12-19 06:42:24 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function (num1, num2) {
+    if (num1 !== undefined && num2 !== undefined) {
+      return num1 / num2;
+    } else {
+      return 2000 / 100;
+    }
+}
+const square= (x) => x * x;
+square (2);
+
+const add = (a, b ) => a+ b;
+add (2 ,3)
